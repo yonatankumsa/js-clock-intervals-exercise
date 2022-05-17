a614b1dc0c97cec9d4f770daf8812576eeead67a
yonatankumsa <yonatankumsa99@gmail.com>
2022-05-16 22:50:40 -0400

    yontans clock commit

---
+++ clock.js
@@ -0,0 +1,25 @@
+const secondEl = document.getElementById("second");
+const minuteEl = document.getElementById("minute");
+const hourEl = document.getElementById("hour");
+const clockEl = document.getElementById("face");
+
+let second = 0;
+let minute = 0;
+let hour = 0;
+let d = new Date();
+function yoni(yoo) {
+  setInterval(function () {
+    d = new Date();
+    second = d.getSeconds() *6;
+    minuits = d.getMinutes() * 6;
+    hour = d.getHours() * 30;
+
+    secondEl.style.transform = "rotate(" + second + "deg)";
+    minuteEl.style.transform = "rotate(" + minuits + "deg)";
+    hourEl.style.transform = "rotate(" + hour + "deg)";
+   
+  }, 1000);
+  
+}
+yoni();
+   
